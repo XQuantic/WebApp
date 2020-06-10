86042abbd80b522121bca8df4c3d04adea4cb65b
XQuantic <sevennweek@gmail.com>
2020-06-10 06:24:40 +0300

    Added validation for Razor component

---
+++ PhoneStore/ViewModels/DeletePhone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneStore.ViewModels
+{
+    public class DeletePhone
+    {
+        [Required(ErrorMessage = "Please enter phone name")]
+        public string Name { get; set; }
+    }
+}
---
+++ PhoneStore/ViewModels/InsertPhone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneStore.ViewModels
+{
+    public class InsertPhone
+    {
+        [Required(ErrorMessage = "Please enter phone name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter phone country")]
+        public string Country { get; set; }
+        [Required(ErrorMessage = "Please enter phone price")]
+        public double Price { get; set; }
+        [Required(ErrorMessage = "Please select company")]
+        public string CompanyId { get; set; }
+    }
+}
